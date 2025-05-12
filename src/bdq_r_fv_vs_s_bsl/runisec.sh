1edcbe1b46423e16b1fe1a564ee73a597609757c
Tristan <tjang8858@rci-valafar-head-01.cm.cluster>
2025-05-12 10:15:14 -0700

    Run bcftools isec to compare follow-up and baseline VCFs for multiple sample pairs

---
+++ src/bdq_r_fv_vs_s_bsl/runisec.sh
@@ -0,0 +1,12 @@
+#cd /grp/valafar/data/depot/assembly/south_africa/variants/isec/
+
+
+bcftools isec ../A04_B124_FV_New_annotated.vcf.gz ../A04_R30124_BSL_New_annotated.vcf.gz -p A04_New_RvS_isec
+bcftools isec ../A14_B183_FV_New_annotated.vcf.gz ../A14_R30185_BSL_New_annotated.vcf.gz -p A14_New_RvS_isec
+bcftools isec ../A10_B158_FV_New_annotated.vcf.gz ../A10_R30990_BSL_New_annotated.vcf.gz -p A10_New_RvS_isec
+bcftools isec ../A12_B180_FV_New_annotated.vcf.gz ../A12_R26939_BSL_New_annotated.vcf.gz -p A12_New_RvS_isec
+bcftools isec ../A25_B272_FV_New_annotated.vcf.gz ../A25_R32047_BSL_New_annotated.vcf.gz -p A25_New_RvS_isec
+bcftools isec ../B07_R36134_FV_New_annotated.vcf.gz ../B07_R31035_BSL_New_annotated.vcf.gz -p B07_New_RvS_isec
+#bcftools isec ../B09_R37765_FV_New_annotated.vcf.gz ../B09_R31038_BSL_New_annotated.vcf.gz -p B09_New_RvS_isec
+#bcftools isec ../A20_B254_FV_New_annotated.vcf.gz ../A20_R29608_BSL_New_annotated.vcf.gz -p A20_New_RvS_isec
+bcftools isec ../A08_B145_FV_New_annotated.vcf.gz ../A08_R30289_BSL_New_annotated.vcf.gz -p A08_New_RvS_isec
